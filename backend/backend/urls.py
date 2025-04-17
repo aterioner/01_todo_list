f5f2d5cb2234077e028d02448af7aea3afdd46cf
Jiří Štancl <stancljiri95@gmail.com>
2025-04-17 21:15:35 +0200

    Debug log: výpis requestů z GET

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,7 +2,13 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from django.http import JsonResponse
+
+def health_check(request):
+    return JsonResponse({"status": "ok"})
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/", include("todo.urls")),
+    path("", health_check),  # 👈 Tohle je důležité!
 ]
