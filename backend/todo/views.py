42b338d360c5298ca4398e866bbe274dd2da9e08
Jiří Štancl <stancljiri95@gmail.com>
2025-04-17 18:12:55 +0200

    Debug log: výpis requestů z GET a POST

--- backend/todo/views.py
+++ backend/todo/views.py
@@ -6,3 +6,13 @@ from .serializers import TaskSerializer
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all().order_by("-created")
     serializer_class = TaskSerializer
+
+    def list(self, request, *args, **kwargs):
+    print("📥 GET request headers:", dict(request.headers))
+    print("📥 GET request content type:", request.content_type)
+    print("📥 GET request accepted:", request.accepted_media_type)
+    return super().list(request, *args, **kwargs)
+
+def create(self, request, *args, **kwargs):
+    print("📤 POST data:", request.data)
+    return super().create(request, *args, **kwargs)
